bd3c0eac1b242a3b4b40bb38a1d8ccfc46c1b7ce
Eden <pkh_eden@photons.com.tw>
2025-02-06 10:07:18 +0800

    Initial Commit

---
+++ LogoSearchTests/LogoSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogoSearchTests.swift
+//  LogoSearchTests
+//
+//  Created by Eden on 2025/2/6.
+//
+
+import Testing
+@testable import LogoSearch
+
+struct LogoSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
